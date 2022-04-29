28ff8774926e05f84836a5c0c5936610d4e5b685
HastalavistaAT <gerd@gerd.at>
2022-04-29 20:21:14 +0200

    added awattar.py

---
+++ python/development/awattar.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+from paho.mqtt import client as mqtt_client
+import time
+import json
+import requests
+
+# settings
+broker = 'localhost'
+port = 1883
+topic = "home/awattar"
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 100)}'
+# username = 'emqx'
+# password = 'public'
+
+def connect_mqtt() -> mqtt_client:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    #client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def publish(message):
+    global client
+    def on_publish(client,userdata,result):             #create function for callback
+        print("data published \n")
+        pass
+    client.on_publish = on_publish                          #assign function to callback
+    client.publish(topic,message)                   #publish
+
+def load_prices():
+    response = requests.get("https://api.awattar.at/v1/marketdata")
+    message = json.loads(response.json())
+    print(message)
+    
+def loop():
+    while True:
+        load_prices()
+        publish("test")
+        time.sleep(3600)
+
+def run():
+    global client
+    client = connect_mqtt()
+    loop()
+
+if __name__ == '__main__':
+    run()
