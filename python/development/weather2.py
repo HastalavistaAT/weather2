dcdb70e953120dd66aa796725d16d6f9185bef73
HastalavistaAT <gerd@gerd.at>
2022-04-29 22:50:14 +0200

    msg fix

--- python/development/weather2.py
+++ python/development/weather2.py
@@ -236,7 +236,8 @@ def draw_display():
             drawred.rectangle((134, 89, 199, 155), fill = 0)
 
         # right area
-        drawblack.text((232, 16), f"{str(round(current_price, 1))}°", font = fontbold24, align='center', fill = 0, anchor="mm")
+        drawblack.text((232, 16), f"{str(round(current_price, 1))}", font = fontbold24, align='center', fill = 0, anchor="mm")
+        drawblack.text((232, 26), f"ct/kWh", font = font18, align='center', fill = 0, anchor="mm")
 
         # bottom line
         drawblack.text((42, 166), "05:58", font = fontbold16, align='center', fill = 0, anchor="mm")
