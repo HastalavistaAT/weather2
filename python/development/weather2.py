b38b7d2e66f58725f7443142183608ca7124411c
HastalavistaAT <gerd@gerd.at>
2022-05-01 21:19:31 +0200

    added debug output

--- python/development/weather2.py
+++ python/development/weather2.py
@@ -267,6 +267,7 @@ def draw_display():
                 drawblack.text((232, 135), f"{today_tomorrow}", font = font16, align='center', fill = 0, anchor="mm")
                 drawblack.text((232, 151), f"{GS_start_time.strftime('%H')} Uhr", font = font16, align='center', fill = 0, anchor="mm")
                 time_diff = datetime.now() - GS_start_time
+                print(format_seconds_to_hhmm(time_diff.total_seconds()))
                 drawblack.text((232, 166), f"T{format_seconds_to_hhmm(time_diff.total_seconds())}", font = fontbold16, align='center', fill = 0, anchor="mm")
                 
 
