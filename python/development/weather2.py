54f5b155b20c4d626438aedfa54c1aa993b02eb1
HastalavistaAT <gerd@gerd.at>
2022-05-01 21:51:49 +0200

    fix

--- python/development/weather2.py
+++ python/development/weather2.py
@@ -283,8 +283,8 @@ def draw_display():
             print("Error when drawing awattar prices")
 
         # bottom line
-        drawblack.text((42, 166), f"{timer.calculate_open_time().strftime('%H:%M')}", font = fontbold16, align='center', fill = 0, anchor="mm")
-        drawblack.text((100, 166), time, font = fontbold16, align='center', fill = 0, anchor="mm")
+        drawblack.text((42, 166), f"{timer.calculate_open_time()}", font = fontbold16, align='center', fill = 0, anchor="mm")
+        drawblack.text((100, 166), time.strftime('%H:%M'), font = fontbold16, align='center', fill = 0, anchor="mm")
         drawblack.text((176, 166), f"{timer.calculate_close_time().strftime('%H:%M')}", font = fontbold16, align='center', fill = 0, anchor="mm")
 
         epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRedimage))
